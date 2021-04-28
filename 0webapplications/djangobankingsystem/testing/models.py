852ce4202fdcca7a5eb7c3bc7253ee0c65c032e1
GustavoPT <gustxw@gmail.com>
2021-04-28 11:23:03 -0700

    t3

--- 0webapplications/djangobankingsystem/testing/models.py
+++ 0webapplications/djangobankingsystem/testing/models.py
@@ -9,30 +9,31 @@ class Seller(models.Model):
     name = models.CharField(max_length=10)
 
 class Product(models.Model):
-    id = models.Field(primary_key=True)
-    price = models.DecimalField
+    # product_id = models.AutoField(primary_key=True)
+    price = models.DecimalField(max_digits=4,decimal_places=4,null=True)
     name = models.CharField(max_length=10)
-    isService = models.BooleanField
+    isService = models.BooleanField(default=False)
     seller = models.ForeignKey(Seller,on_delete=models.CASCADE)
+
 class Card(models.Model):
-    id = models.Field(primary_key=True)
     number = models.IntegerField()
     month = models.IntegerField()
     year = models.IntegerField()
     backcode = models.IntegerField()
     image = models.ImageField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
 
 class Transaction(models.Model):
     id = models.Field(primary_key=True)
     card = models.ForeignKey(Card,on_delete=models.CASCADE)
     seller = models.ForeignKey(Seller,on_delete=models.CASCADE)
-    amount = models.DecimalField 
+    amount = models.DecimalField(max_digits=4,decimal_places=4,null=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
 
 class Account(models.Model):
     id = models.Field(primary_key=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    balance = models.DecimalField 
+    balance = models.DecimalField(max_digits=4,decimal_places=4,null=True)
 
     
 
